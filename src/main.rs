42e45eacf7f999c6ae285841604493d59fb64e8e
gatcuddy <emmettglaser@gmail.com>
2021-03-05 17:32:55 -0800

    bilinear sampling

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,58 @@
 
 use reqwest::{Client};
 use serde::Deserialize;
-use image::{DynamicImage, GenericImageView, ImageFormat, Rgba, EncodableLayout, load_from_memory, GenericImage};
+use image::{DynamicImage, GenericImageView, ImageFormat, Rgba, EncodableLayout, load_from_memory, GenericImage, Rgb, RgbImage};
 use image::imageops::{FilterType};
 use uuid::Uuid;
 use std::fs::create_dir;
 use std::path::Path;
 use std::convert::From;
-use std::fs;
-use std::cmp::min;
+use std::{fs, env};
 
 const ASPECT: f32 = 4.0f32 / 3.0f32;
 const IMAGE_DIR: &str = "./cardImages/";
-const BASE_IMAGE_DIR: &str = "./test/real scale.png";
+const BASE_IMAGE_DIR: &str = "./test/rosa head crop.png";
 
 #[tokio::main]
 async fn main() {
-	//save_num_cards(IMAGE_DIR, &mut card_images, ASPECT, 16).await.unwrap();
+	/* see what we're gonna do for this run */
+	let args: Vec<String> = env::args().collect();
 
-	setup_dir(IMAGE_DIR).unwrap();
+	if args.len() == 1 {
+		println!("No arguments provided!");
 
-	let mut card_images: Vec<DynamicImage> = Vec::with_capacity(32);
+	} else if args[1] == "pull" {
+		let mut card_images: Vec<DynamicImage> = Vec::new();
+		save_num_cards(IMAGE_DIR, &mut card_images, ASPECT, 9).await.unwrap();
 
-	load_existing_images(IMAGE_DIR, &mut card_images);
+	} else if args[1] == "resample" {
+		setup_dir(IMAGE_DIR).unwrap();
 
-	println!("Loaded {} images!", card_images.len());
+		let mut card_images: Vec<DynamicImage> = Vec::with_capacity(32);
 
-	let base_image = image::load_from_memory(fs::read(BASE_IMAGE_DIR).unwrap().as_slice()).unwrap();
+		load_existing_images(IMAGE_DIR, &mut card_images);
 
-	println!("Loaded base image!");
+		println!("Loaded {} images!", card_images.len());
 
-	let mut card_grid = create_grid(128, ASPECT, base_image.width(), base_image.height());
+		let base_image = image::load_from_memory(fs::read(BASE_IMAGE_DIR).unwrap().as_slice()).unwrap().to_rgb8();
 
-	populate_grid(&base_image, &card_images, &mut card_grid, ASPECT);
+		println!("Loaded base image!");
 
-	println!("Found cards to sample!");
+		let mut card_grid = create_grid(72, ASPECT, base_image.width(), base_image.height());
 
-	let output_image = draw_cards(&card_grid, &card_images, ASPECT, 2000);
+		populate_grid(&base_image, &card_images, &mut card_grid, ASPECT);
 
-	println!("Drew sampled image!");
+		println!("Found cards to sample!");
 
-	output_image.save_with_format("./test/sampled.png", ImageFormat::Png).unwrap();
+		let output_image = draw_cards(&card_grid, &card_images, ASPECT, 1800);
+
+		println!("Drew sampled image!");
+
+		output_image.save_with_format("./test/sampled.png", ImageFormat::Png).unwrap();
+
+	} else {
+		println!("Invalid argument");
+	}
 }
 
 #[derive(Deserialize)]
@@ -75,9 +87,11 @@ fn load_existing_images(dir_path: &str, card_images: &mut Vec<DynamicImage>) {
 async fn save_num_cards(dir_path: &str, card_images: &mut Vec<DynamicImage>, card_aspect: f32, num_cards: u32) -> Result<(), Box<dyn std::error::Error>> {
 	let client = reqwest::Client::new();
 
-	for c in 0..num_cards {
+	for _ in 0..num_cards {
 		let (card_image, card_uuid) = get_card(&client).await?;
 		save_card(card_image, card_uuid, card_aspect, dir_path, card_images);
+
+		println!("Got card {}!", card_uuid.to_string());
 	}
 
 	Ok(())
@@ -142,12 +156,24 @@ struct CardGrid {
 	cards_tall: u32
 }
 
-fn populate_grid(base_image: &DynamicImage, card_images: &Vec<DynamicImage>, card_grid: &mut CardGrid, card_aspect: f32) {
+fn populate_grid(base_image: &RgbImage, card_images: &Vec<DynamicImage>, card_grid: &mut CardGrid, card_aspect: f32) {
+	let card_samples = create_card_samples(card_images, card_aspect, base_image.width(), card_grid.cards_wide);
+
 	for x in 0..card_grid.cards_wide {
 		for y in 0..card_grid.cards_tall {
-			card_grid.grid[(y * card_grid.cards_wide + x) as usize] = select_best_card(&base_image, &card_images, &card_grid, x, y);
+			card_grid.grid[(y * card_grid.cards_wide + x) as usize] = select_best_card(&base_image, &card_samples, &card_grid, x, y);
 		}
 	}
+}
+
+fn create_card_samples(card_images: &Vec<DynamicImage>, card_aspect: f32, image_width: u32, cards_wide: u32) -> Vec<RgbImage> {
+	let sample_width = (image_width as f32 / cards_wide as f32).round() as u32;
+	let sample_height = ((1f32 / card_aspect) * sample_width as f32).round() as u32;
+
+	card_images
+		.iter()
+		.map(|full_image| full_image.resize_exact(sample_width, sample_height, FilterType::CatmullRom).to_rgb8())
+		.collect::<Vec<RgbImage>>()
 }
 
 fn create_grid(cards_wide: u32, card_aspect: f32, image_width: u32, image_height: u32) -> CardGrid {
@@ -161,33 +187,52 @@ fn create_grid(cards_wide: u32, card_aspect: f32, image_width: u32, image_height
 	CardGrid { grid, cards_wide, cards_tall }
 }
 
-fn select_best_card(base_image: &DynamicImage, card_images: &Vec<DynamicImage>, card_grid: &CardGrid, x: u32, y: u32) -> u32 {
+fn select_best_card(base_image: &RgbImage, card_images: &Vec<RgbImage>, card_grid: &CardGrid, x: u32, y: u32) -> u32 {
 	let region_width = base_image.width() as f32 / card_grid.cards_wide as f32;
 	let region_height = base_image.height() as f32 / card_grid.cards_tall as f32;
 
 	let region_x = region_width * x as f32;
 	let region_y = region_height * y as f32;
 
-	let min_x = region_x.floor() as u32;
-	let max_x = ((region_x + region_width).ceil() as u32).min(base_image.width() - 1);
+	let min_x = region_x.round() as u32;
+	let max_x = ((region_x + region_width).round() as u32).min(base_image.width() - 1);
 	let len_x = max_x - min_x + 1;
 
-	let min_y = region_y.floor() as u32;
-	let max_y = ((region_y + region_height).ceil() as u32).min(base_image.height() - 1);
+	let min_y = region_y.round() as u32;
+	let max_y = ((region_y + region_height).round() as u32).min(base_image.height() - 1);
 	let len_y = max_y - min_y + 1;
 
-	fn dimension_weight(i: u32, int_min: u32, int_max: u32, min: f32, max: f32) -> f32 {
-		match i {
-			i if i == int_min => (int_min as f32 + 1.0f32) - min,
-			i if i == int_max => max - (int_max as f32 - 1.0f32),
-			_ => 1.0f32
-		}
+	fn bilinear(bytes: &[u8], width: u32, height: u32, x: f32, y: f32) -> [u8; 3] {
+		let pixel_x0 = x as u32;
+		let pixel_x1 = (pixel_x0 + 1u32).min(width - 1);
+		let weight_x1 = pixel_x0 as f32 - x;
+		let weight_x0 = 1f32 - weight_x1;
+
+		let pixel_y0 = y as u32;
+		let pixel_y1 = (pixel_y0 + 1u32).min(height - 1);
+		let weight_y1 = pixel_y0 as f32 - y;
+		let weight_y0 = 1f32 - weight_y1;
+
+		let channel_weight = |offset: u32| -> u8 {
+			(
+				(bytes[((pixel_y0 * width + pixel_x0) * 3u32 + offset) as usize] as f32 * weight_x0 * weight_y0) +
+				(bytes[((pixel_y1 * width + pixel_x0) * 3u32 + offset) as usize] as f32 * weight_x0 * weight_y1) +
+				(bytes[((pixel_y0 * width + pixel_x1) * 3u32 + offset) as usize] as f32 * weight_x1 * weight_y0) +
+				(bytes[((pixel_y1 * width + pixel_x1) * 3u32 + offset) as usize] as f32 * weight_x1 * weight_y1)
+			).round() as u8
+		};
+
+		[channel_weight(0), channel_weight(1), channel_weight(2)]
 	}
 
-	fn pixel_compare(pixel0: Rgba<u8>, pixel1: Rgba<u8>) -> u32 {
-		(pixel0.0[0] as i16 - pixel1.0[0] as i16).abs() as u32 +
-		(pixel0.0[1] as i16 - pixel1.0[1] as i16).abs() as u32 +
-		(pixel0.0[2] as i16 - pixel1.0[2] as i16).abs() as u32
+	fn direct(bytes: &[u8], width: u32, x: u32, y: u32) -> [u8; 3] {
+		[bytes[((width * y + x) * 3u32) as usize], bytes[((width * y + x) * 3u32 + 1) as usize], bytes[((width * y + x) * 3u32 + 2) as usize]]
+	}
+
+	fn pixel_compare(pixel0: [u8; 3], pixel1: [u8; 3]) -> u32 {
+		(pixel0[0] as i16 - pixel1[0] as i16).abs() as u32 +
+		(pixel0[1] as i16 - pixel1[1] as i16).abs() as u32 +
+		(pixel0[2] as i16 - pixel1[2] as i16).abs() as u32
 	}
 
 	let mut least_dif = u32::max_value();
@@ -199,17 +244,16 @@ fn select_best_card(base_image: &DynamicImage, card_images: &Vec<DynamicImage>, 
 		/* all pixels sampled from the card image */
 		for x in 0..len_x {
 			let base_x = x + min_x;
-			let weight_x = dimension_weight(base_x, min_x, max_x, region_x, region_x + region_width);
-			let card_x = ((x as f32 / len_x as f32) * card_image.width() as f32) as u32;
+			let card_x = (x as f32 / len_x as f32) * card_image.width() as f32;
 
 			for y in 0..len_y {
 				let base_y = y + min_y;
-				let weight_y = dimension_weight(base_y, min_y, max_y, region_y, region_y + region_height);
-				let card_y = ((y as f32 / len_y as f32) * card_image.height() as f32) as u32;
+				let card_y = (y as f32 / len_y as f32) * card_image.height() as f32;
 
-				let weight = weight_x * weight_y;
-
-				current_dif += (pixel_compare(base_image.get_pixel(base_x, base_y), card_image.get_pixel(card_x, card_y)) as f32 * weight) as u32;
+				current_dif += pixel_compare(
+					direct(base_image.as_bytes(), base_image.width(), base_x, base_y),
+					bilinear(card_image.as_bytes(), card_image.width(), card_image.height(), card_x, card_y)
+				);
 			}
 		}
 
